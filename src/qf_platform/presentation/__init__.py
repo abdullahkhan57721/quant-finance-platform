ae0dae30a8c45fd6eb3480a7dd1ab7e3ab182968
Abdullah Khan <137455050+abdullahkhan57721@users.noreply.github.com>
2026-09-08 17:12:50 -0400

    UI3: export hedging and market presentation adapters

--- src/qf_platform/presentation/__init__.py
+++ src/qf_platform/presentation/__init__.py
@@ -13,11 +13,30 @@ from qf_platform.presentation.m2_workbench import (
     build_m2_workbench_presentation,
 )
 from qf_platform.presentation.plotting import PlotData, PlotPoint, PlotSeries
+from qf_platform.presentation.ui3_hedging import (
+    HedgeFrequencyRow,
+    HedgeStepRow,
+    HedgeWorkbenchPresentation,
+    build_hedge_workbench_presentation,
+    hedge_step_detail_rows,
+)
+from qf_platform.presentation.ui3_market import (
+    MarketObservationDetail,
+    MarketObservationRow,
+    MarketWorkbenchPresentation,
+    build_market_workbench_presentation,
+)
 
 __all__ = [
     "BlackScholesPresentation",
     "GreekComparisonRow",
+    "HedgeFrequencyRow",
+    "HedgeStepRow",
+    "HedgeWorkbenchPresentation",
     "M2WorkbenchPresentation",
+    "MarketObservationDetail",
+    "MarketObservationRow",
+    "MarketWorkbenchPresentation",
     "PayoffPoint",
     "PlotData",
     "PlotPoint",
@@ -25,5 +44,8 @@ __all__ = [
     "PresentationRow",
     "ValuationComparisonRow",
     "build_black_scholes_presentation",
+    "build_hedge_workbench_presentation",
     "build_m2_workbench_presentation",
+    "build_market_workbench_presentation",
+    "hedge_step_detail_rows",
 ]
